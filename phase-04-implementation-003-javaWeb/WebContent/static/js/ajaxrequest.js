b55c382793b18b001a2b8d264b0a06d2b6a35798
Yuanzhe Liu <liuyuanzhe0515@gmail.com>
2020-01-26 20:18:47 +0800

    图书管理系统：bootstrap美化界面

--- phase-04-implementation-003-javaWeb/WebContent/static/js/ajaxrequest.js
+++ phase-04-implementation-003-javaWeb/WebContent/static/js/ajaxrequest.js
@@ -11,18 +11,18 @@ function ajaxRequest(type, url, data, href, func) {
 		success : function(result, testStatus) {
 			if (result.code == "200") {
 				//命令执行成功跳转
-				alert(result.message);
+				alertjs(result);
 				if (href!=null)
 					window.location.href = href;
 			}
 			if (result.code == "605" && href!=null) window.location.href = href;
 			if (result.code == "403") {
 				//命令执行失败，或有错误消息需要显示
-				alert(result.message);
+				alertjs(result);
 			}
 			if (result.code == "601") {
 				//未登录
-				alert(result.message);
+				alertjs(result);
 				window.location.href = './';
 			}
 			if (func!=null) 
@@ -36,4 +36,36 @@ function ajaxRequest(type, url, data, href, func) {
 }
 function logout(){
 	ajaxRequest("get","api/logout",null,"index.html",null);
+}
+//alert弹窗需要alert组件
+function alertjs(result){
+	var fadetimes = 300,showtimes = 3000;
+	$("#alert").removeClass("alert-success");
+	$("#alert").removeClass("alert-info");
+	$("#alert").removeClass("alert-warning");
+	$("#alert").removeClass("alert-danger");
+	if (result.code==200) 
+		$("#alert").addClass("alert-success");
+	else if (result.code==403) 
+		$("#alert").addClass("alert-danger");
+	else $("#alert").addClass("alert-info");
+	
+	$("#alert").show(fadetimes);
+	$("#alert").text(result.message);
+	setTimeout( function(){
+		$("#alert").hide(fadetimes)
+	},showtimes);
+}
+//回调函数
+function callBack(result){
+	if (result.code == 200) {
+		search();
+	}
+}
+function callBackReset(result) {
+	if (result.code == 200) {
+		$('#form1')[0].reset();
+		$('#savebtn').hide();	
+		search();
+	}
 }
